4a92994775c30d65b1ab51f4190291762b61ae21
Mark Cooper <mark.c.cooper@outlook.com>
2021-11-22 15:54:45 -0800

    Prep for webhook data

--- helpers/webhook.rb
+++ helpers/webhook.rb
@@ -17,17 +17,24 @@ class ASpaceInsightsApi < Sinatra::Application
                              error: 'Invalid payload'
                            })]
     rescue WebhookError => e
-      [500, MultiJson.dump({
+      [400, MultiJson.dump({
                              error: e.message
                            })]
     end
 
     def handle_report(event)
-      data     = event['data']
-      # instance = data['header']
-      # global   = data['report']['global']
-      # repo     = data['report']['repository']
-      puts data.inspect
+      data        = event['data']
+      # instance    = data['header']
+      # global      = data['report']['global']
+      # repoitories = data['report']['repository']
+      # i = Instance.find_or_create_by(sitecode: instance['sitecode'])
+      # i.update_attributes(MultiJson.load(instance))
+      # i.reports << Report.create(MultiJson.load(global))
+      # repoitories.each do |code, report|
+      #   i.repositories << Repository.find_or_create_by(code: code) do |r|
+      #     r.reports << Report.create(MultiJson.load(report))
+      #   end
+      # end
     end
   end
 end
--- routes/instances.rb
+++ routes/instances.rb
@@ -1,8 +1,5 @@
 class ASpaceInsightsApi < Sinatra::Application
   post '/instances' do
     ASpaceInsightsApi::Webhook.new.handle(request)
-    # MultiJson.dump({
-    #                  result: 'ok'
-    #                })
   end
 end
